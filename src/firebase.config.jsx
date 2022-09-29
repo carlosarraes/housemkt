372906422b15e92fc84c099bd8833ab1e71cead9
carraes <carraeshb@gmail.com>
2022-09-28 22:07:42 -0300

    Estrutura feita

---
+++ src/firebase.config.jsx
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyANJzT4-BsqdJrRrTHzX9vAAAusJwXpMd0',
+  authDomain: 'house-marketplace-app-2b5fc.firebaseapp.com',
+  projectId: 'house-marketplace-app-2b5fc',
+  storageBucket: 'house-marketplace-app-2b5fc.appspot.com',
+  messagingSenderId: '527572105222',
+  appId: '1:527572105222:web:f3f7db4126117ac8c8eb87',
+};
+
+// Initialize Firebase
+
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+const SignUp = () => {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  );
+};
+
+export default SignUp;
